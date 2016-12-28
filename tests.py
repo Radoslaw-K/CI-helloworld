7b585fdef8ec2e902a44604e09af33873ae4149d
Radoslaw Kieltyka <Radoslaw-K@users.noreply.github.com>
2016-12-28 00:34:27 +0000

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import main
 
 class MyTest(unittest.TestCase):
     def test_main(self):
-        self.assertEqual(main.main(), "Hello World")
+        self.assertEqual(main.main(0), "Hello World")
 
 if __name__ == '__main__':
     unittest.main()
